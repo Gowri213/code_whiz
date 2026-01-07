957f19b3ced1dfb44dd953de749b00d752ab2a5c
Gowri <gowri21115@gmail.com>
2026-01-07 13:29:42 +0530

    Update at.py

--- at.py
+++ at.py
@@ -1,14 +1,14 @@
-total_classes = int(input("Enter total classes: "))
-class_attended = int(input("Enter no.of classes attended: "))
+total_classes = float(input("Enter total classes: "))
+class_attended = float(input("Enter no.of classes attended: "))
 
 attendance_percentage = (class_attended/total_classes)*100
-
+per = (total_classes*3/4) - class_attended
 print(f"Attendance percentage: {attendance_percentage}%")
 
 if attendance_percentage < 75:
     print("Not Eligible")
-    additional_classes = total_classes - class_attended - 20
-    print(f"Additional classes required: {additional_classes}")
+    additional_classes = total_classes - class_attended 
+    print(f"Additional classes required: {per}")
 else:
     print("Eligible")
 
