ef00cd92464d6ee366d52358ec4ae42f7c93f8d3
Gowri <gowri21115@gmail.com>
2026-01-07 16:14:42 +0530

    Update ps.py

--- ps.py
+++ ps.py
@@ -1,8 +1,28 @@
 password = input("Enter the password: ")
-
+upper = False
+lower = False
+digit = False
+special_character = False
+special = "+^%$#@*!&"
 
 if len(password) >= 8:
-        if password.isalnum():
-            print("strong")
+    for ch in password:
+        if ch.isupper():
+            upper = True
+        if ch.islower():
+            lower = True
+    
+        if ch.isdigit():
+            digit = True
+        if ch in special:
+            special_character = True
 else:
-    print("weak")
+    print("Password must contain atleast 8 characters")
+
+
+if upper & lower & digit & special_character:
+    print("Strong")
+elif upper & lower & digit:
+    print("Medium")
+else:
+    print("Weak")
